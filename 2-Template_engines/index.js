4cfb9e21653de6bf8637811c0546158d1a634bb9
AnhPham <th3anh1808@gmail.com>
2019-12-22 23:07:35 +0700

    Query parameters

--- 2-Template_engines/index.js
+++ 2-Template_engines/index.js
@@ -4,6 +4,12 @@ const app = express();
 
 const port = 3000;
 
+var users = [
+	{id :1 , name : 'The Anh'},
+	{id :2 , name: 'Thanh'},
+	{id :3 , name: 'Hoang'},
+	{id :4 , name: 'Luc'}
+]
 app.set('view engine', 'pug');
 app.set('views', './views');
 
@@ -15,13 +21,20 @@ app.get('/', function (req,res){
 
 app.get('/user', function(req, res){
 	res.render('user/index', {
-		users: [
-			{id :1 , name : 'The Anh'},
-			{id :2 , name: 'Thanh'}
-		]
+		users: users
+	});
+});
+
+app.get('/user/search', function(req, res){
+	var q = req.query.q;
+	var fitleUser = users.filter(function(user){
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+	});
+	res.render('user/index', {
+		users : fitleUser
 	});
 });
 
 app.listen(port ,function(){
-	console.log('Example app listening on port ' + port)
+	console.log('Example app listening on port ' + port);
 });
