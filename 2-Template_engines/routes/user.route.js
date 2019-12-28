7065b22edef0f1f9d62f83d95f1c718b3ba6ed21
AnhPham <th3anh1808@gmail.com>
2019-12-29 00:18:28 +0700

    lesson-14 Cookie

--- 2-Template_engines/routes/user.route.js
+++ 2-Template_engines/routes/user.route.js
@@ -7,6 +7,11 @@ router.get('/', controller.index);
 
 router.get('/search', controller.search);
 
+router.get('/cookie'function(req, res, next){
+	res.cookie('user-id',12345);
+	res.send('Hello');
+}):
+
 router.get('/create', controller.create);
 
 router.get('/:id', controller.getId);
